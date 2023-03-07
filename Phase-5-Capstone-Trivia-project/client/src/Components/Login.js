4efc9600b21b32ac9a4a6a925e696e22f2ac1fda
john Caropino <jccaropino7786@live.com>
2023-03-07 11:39:31 -0800

    commit

--- Phase-5-Capstone-Trivia-project/client/src/Components/Login.js
+++ Phase-5-Capstone-Trivia-project/client/src/Components/Login.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Form from "react-bootstrap/Form";
+import {Form, Input, TextArea, Button, Select} from "semantic-ui-react"
 import { useNavigate } from "react-router-dom";
 
 function LogIn({setCurrentUser, setLogin}){
