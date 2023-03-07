388b13002114820e11505efa2bf2f973defe32f2
john Caropino <jccaropino7786@live.com>
2023-03-07 10:42:13 -0800

    background

--- Phase-5-Capstone-Trivia-project/client/src/App.js
+++ Phase-5-Capstone-Trivia-project/client/src/App.js
@@ -1,9 +1,9 @@
-
-import './App.css';
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         Good Head
       </header>
