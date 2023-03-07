ed056da44b49e74be059aa064d367c476cec80f3
john Caropino <jccaropino7786@live.com>
2023-03-07 10:13:28 -0800

    user model

--- Phase-5-Capstone-Trivia-project/app/models/user.rb
+++ Phase-5-Capstone-Trivia-project/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :email, presence: true, uniqueness:true
+    validates :username, presence: true, uniqueness:true
+    validates :password, presence: true, length: { minimum: 8 }, on: :create
+
 end
