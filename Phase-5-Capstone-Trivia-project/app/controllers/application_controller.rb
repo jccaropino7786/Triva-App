29f99939bd3c916475995faa5cb2b3ea7f15ed26
john Caropino <jccaropino7786@live.com>
2023-03-07 10:07:10 -0800

    commit

--- Phase-5-Capstone-Trivia-project/app/controllers/application_controller.rb
+++ Phase-5-Capstone-Trivia-project/app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+        rescue_from ActiveRecord::RecordNotFound, with: :not_found
+        rescue_from ActiveRecord::RecordInvalid, with: :not_valid
+
+        before_action :authorized_user
+
+        def current_user
+            @user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+        end
+
+        def authorized_user
+            render json: {errors: "unauthorized"}, status: :unauthorized unless current_user
+        end
+
+        private
+
+        def not_found(invalid)
+            render json: {error: "#{invalid.model} not found"}, status: :not_found
+        end
+
+        def not_valid(invalid)
+            render json: {errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+        end
 
 end
--- Phase-5-Capstone-Trivia-project/app/controllers/users_controller.rb
+++ Phase-5-Capstone-Trivia-project/app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [ :update, :destroy]
+    skip_before_action :authorized_user, only: [:create]
+
+    def index
+        render json: User.all, status: :ok
+    end
+    
+    def show
+        render json: @user
+    end
+
+    def create
+        new_user = User.create!(user_params)
+        session[:user_id] = new_user.id
+        render json: new_user, status: :created
+    end
+
+    def update
+        render json: @user.update!(user_params), status: :ok
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+    private
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(:email, :username, :password)
+    end
 end
