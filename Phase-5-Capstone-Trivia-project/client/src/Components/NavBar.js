a833db2bbf9bdf99d0e1ce40eba6e45f8e9d7c76
john Caropino <jccaropino7786@live.com>
2023-03-07 10:29:27 -0800

    navbar

---
+++ Phase-5-Capstone-Trivia-project/client/src/Components/NavBar.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+function NavBar({currentUser, setCurrentUser}){
+
+    const logout = () => {
+        
+        fetch('/logout', { method: 'DELETE' })
+            .then(() => setCurrentUser(null))
+    }
+
+    return(
+            <div id = "links">
+            <ul className="nav" >
+                <li><Link to="">High Scores</Link></li>
+                <li><Link to="">New Game</Link></li>
+                <li><Link to="">Profile</Link></li>
+
+                { currentUser ? (<li onClick={logout}> Logout {currentUser.email} </li>) : (
+                    <>
+                    <Link to="/login">Login</Link><br/>
+                    </>
+                    )}
+
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
