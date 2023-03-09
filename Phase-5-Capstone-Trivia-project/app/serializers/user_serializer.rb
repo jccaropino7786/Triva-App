dcd1b3053529b318ecf06f9ec230da61f3bd663d
john Caropino <jccaropino7786@live.com>
2023-03-09 14:22:19 -0800

    fixed

--- Phase-5-Capstone-Trivia-project/app/serializers/user_serializer.rb
+++ Phase-5-Capstone-Trivia-project/app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username
+  attributes :id, :email, :password_digest, :username
 end
--- Phase-5-Capstone-Trivia-project/client/src/index.js
+++ Phase-5-Capstone-Trivia-project/client/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <BrowserRouter>
     {/* <React.StrictMode> */}
-      <App />
+      <App className='main' />
     {/* </React.StrictMode> */}
   </BrowserRouter>,
   document.getElementById('root')
