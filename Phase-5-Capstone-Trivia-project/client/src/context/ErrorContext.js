56dcea43c625ea5f9374692074824f89ba7c1da2
john Caropino <jccaropino7786@live.com>
2023-03-15 08:48:16 -0700

    useContext

---
+++ Phase-5-Capstone-Trivia-project/client/src/context/ErrorContext.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const ErrorContext = React.createContext();
+
+function ErrorProvider({ children }) {
+  const [error, setError] = useState("");
+  return (
+    <ErrorContext.Provider value={{ error, setError }}>
+      {children}
+    </ErrorContext.Provider>
+  );
+}
+
+export { ErrorContext, ErrorProvider };
--- Phase-5-Capstone-Trivia-project/client/src/index.js
+++ Phase-5-Capstone-Trivia-project/client/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ErrorProvider } from './context/ErrorContext';
 
 ReactDOM.render(
-  <BrowserRouter>
-    {/* <React.StrictMode> */}
-      <App className='main' />
-    {/* </React.StrictMode> */}
-  </BrowserRouter>,
+    <ErrorProvider>
+      <BrowserRouter>
+        {/* <React.StrictMode> */}
+          <App className='main' />
+        {/* </React.StrictMode> */}
+      </BrowserRouter>
+    </ErrorProvider>,
   document.getElementById('root')
 );
 
